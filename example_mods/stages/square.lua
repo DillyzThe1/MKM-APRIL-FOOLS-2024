03b669197ac592d73bdc44fc06a55ff455e5dffa
DillyzThe1 <dyl1029an@gmail.com>
2023-01-14 19:11:53 -0600

    square asset update

---
+++ example_mods/stages/square.lua
@@ -0,0 +1,27 @@
+function onCreatePost()
+	makeLuaSprite('bg','bgs/square/bg',-450,-450)
+	setProperty('bg.antialiasing',true)
+	addLuaSprite('bg',false)
+	
+	makeLuaSprite('floor','bgs/square/floor',-1075,550)
+	setProperty('floor.antialiasing',true)
+	
+	makeLuaSprite('floorbehind','bgs/square/floor',-1075,550)
+	setProperty('floorbehind.color', 0)
+	
+	addLuaSprite('floorbehind', false)
+	addLuaSprite('floor',false)
+	
+	setProperty('gf.visible', false)
+end
+
+local etotal = 0
+
+function onUpdatePost(e)
+	etotal = etotal + e
+	
+	setProperty('bg.scale.y', 0.75 + math.sin(etotal*0.25)/4)
+	setProperty('bg.y', -375 + math.sin(etotal*0.25)*100)
+	
+	setProperty('floor.alpha', 0.75 - math.sin(etotal*0.25)/4)
+end
