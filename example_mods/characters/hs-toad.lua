49cf351a67722f8a904aae17e9776dc20b160f18
DillyzThe1 <dyl1029an@gmail.com>
2023-02-07 17:41:18 -0600

    hell shrooms perspective

--- example_mods/characters/hs-toad.lua
+++ example_mods/characters/hs-toad.lua
@@ -1,5 +1,7 @@
+local ogX = 0
 local ogY = 0
 function onCreatePost()
+	ogX = getProperty('dad.x')
 	ogY = getProperty('dad.y')
 end
 
@@ -7,6 +9,8 @@ local totalUpdate = 0
 function onUpdatePost(e)
 	if getProperty('dad.curCharacter') == 'hs-toad' then
 		totalUpdate = totalUpdate + e
-		setProperty('dad.y',ogY + math.sin(totalUpdate)*35)
+		setProperty('dad.x',ogX + math.cos(totalUpdate*1.75)*30)
+		setProperty('dad.y',ogY + math.sin(totalUpdate*1.75)*65)
+		setProperty('dad.angle',math.cos(totalUpdate*1.75)*5)
 	end
 end
