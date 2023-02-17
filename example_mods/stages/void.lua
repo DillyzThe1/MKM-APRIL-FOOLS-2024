b2e62b57a3ab9988b729fbdafa1dc605c205220f
DillyzThe1 <dyl1029an@gmail.com>
2023-02-17 16:07:27 -0600

    bottom text

--- example_mods/stages/void.lua
+++ example_mods/stages/void.lua
@@ -1,8 +1,22 @@
 function onCreatePost()
-	setProperty('boyfriend.visible',false)
-	setProperty('boyfriend.active',false)
-	setProperty('gf.visible',false)
-	setProperty('gf.active',false)
-	setProperty('dad.visible',false)
-	setProperty('dad.active',false)
+	youKnowWhoElse('boyfriend')
+	youKnowWhoElse('gf')
+	youKnowWhoElse('dad')
+	
+	youKnowWhoElse('iconP1')
+	youKnowWhoElse('iconP2')
+	youKnowWhoElse('healthBar')
+	youKnowWhoElse('healthBarBG')
+	youKnowWhoElse('timeTxt')
+	youKnowWhoElse('scoreTxt')
+	
+	setProperty('camGame.alpha', 0)
+	setProperty('camHUD.alpha', 0)
 end
+
+local myMom = 0
+function youKnowWhoElse(isAFunction)
+	setProperty(isAFunction .. '.visible',false)
+	setProperty(isAFunction .. '.active',false)
+	return myMom
+end 
