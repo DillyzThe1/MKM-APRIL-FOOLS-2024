fd43dfb76f07a0f57cbc06178aff976306d73911
DillyzThe1 <dyl1029an@gmail.com>
2023-02-20 16:02:48 -0600

    yeah 2

--- source/StageData.hx
+++ source/StageData.hx
@@ -1,14 +1,14 @@
 package;
 
 #if MODS_ALLOWED
-import sys.io.File;
 import sys.FileSystem;
+import sys.io.File;
 #else
 import openfl.utils.Assets;
 #end
+import Song;
 import haxe.Json;
 import haxe.format.JsonParser;
-import Song;
 
 using StringTools;
 
@@ -32,40 +32,16 @@ class StageData {
 	public static var forceNextDirectory:String = null;
 	public static function loadDirectory(SONG:SwagSong) {
 		var stage:String = '';
-		if(SONG.stage != null) {
+		if(SONG.stage != null)
 			stage = SONG.stage;
-		} else if(SONG.song != null) {
-			switch (SONG.song.toLowerCase().replace(' ', '-'))
-			{
-				case 'spookeez' | 'south' | 'monster':
-					stage = 'spooky';
-				case 'pico' | 'blammed' | 'philly' | 'philly-nice':
-					stage = 'philly';
-				case 'milf' | 'satin-panties' | 'high':
-					stage = 'limo';
-				case 'cocoa' | 'eggnog':
-					stage = 'mall';
-				case 'winter-horrorland':
-					stage = 'mallEvil';
-				case 'senpai' | 'roses':
-					stage = 'school';
-				case 'thorns':
-					stage = 'schoolEvil';
-				case 'ugh' | 'guns' | 'stress':
-					stage = 'tank';
-				default:
-					stage = 'stage';
-			}
-		} else {
+		else
 			stage = 'stage';
-		}
 
 		var stageFile:StageFile = getStageFile(stage);
-		if(stageFile == null) { //preventing crashes
+		if(stageFile == null)
 			forceNextDirectory = '';
-		} else {
+		else
 			forceNextDirectory = stageFile.directory;
-		}
 	}
 
 	public static function getStageFile(stage:String):StageFile {
@@ -74,20 +50,16 @@ class StageData {
 
 		#if MODS_ALLOWED
 		var modPath:String = Paths.modFolders('stages/' + stage + '.json');
-		if(FileSystem.exists(modPath)) {
+		if(FileSystem.exists(modPath))
 			rawJson = File.getContent(modPath);
-		} else if(FileSystem.exists(path)) {
+		else if(FileSystem.exists(path))
 			rawJson = File.getContent(path);
-		}
 		#else
-		if(Assets.exists(path)) {
+		if(Assets.exists(path))
 			rawJson = Assets.getText(path);
-		}
 		#end
 		else
-		{
 			return null;
-		}
 		return cast Json.parse(rawJson);
 	}
 }
