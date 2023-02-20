c42bac042a49bcc2fcb0152c10f29ad160f7b33a
DillyzThe1 <dyl1029an@gmail.com>
2023-02-19 18:49:11 -0600

    better no shrooms drop

--- example_mods/data/no-shrooms/script.lua
+++ example_mods/data/no-shrooms/script.lua
@@ -37,37 +37,43 @@ function onBeatHit()
 	if string.lower(difficultyName) == "hard" then
 		if curBeat >= 164 and curBeat < 228 then 
 			--hardBeatHit(25,1.1)
-			setProperty('camZooming',true)
-			setProperty('camGame.zoom',1.1)
-			setProperty('camHUD.zoom',1.05)
+			setProperty('camZooming',false)
+			setProperty('camGame.zoom',1.275)
+			setProperty('camHUD.zoom',1.175)
 			local newAngle = 3.5
 			if curBeat % 2 == 0 then
 				newAngle = -newAngle
 			end
-			doTweenX('camHUDX', 'camHUD', -newAngle*3, crochet*0.001, 'cubeInOut')
-			doTweenX('camGameX', 'camGame', -newAngle*5, crochet*0.001, 'cubeInOut')
+			setProperty('camGame.angle',newAngle)
+			setProperty('camHUD.angle',newAngle)
 			
-			--setProperty('camGame.angle',newAngle)
-			--setProperty('camHUD.angle',newAngle)
-			
-			doTweenAngle('camHUDAngle', 'camHUD', newAngle, stepCrochet*0.002, 'circOut')
-			doTweenAngle('camGameAngle', 'camGame', newAngle/2, stepCrochet*0.002, 'circOut')
+			local timetotake = stepCrochet*0.0075
+			doTweenAngle('camHUDAngle', 'camHUD', 0, timetotake, 'cubeOut')
+			doTweenAngle('camGameAngle', 'camGame', 0, timetotake, 'cubeOut')
+			doTweenZoom('camHUDZoom', 'camHUD', 1, timetotake, 'cubeOut')
+			doTweenZoom('camGameZoom', 'camGame', 1, timetotake, 'cubeOut')
 			
 			--if curBeat % 2 == 0 then
 				for i = 0, getProperty('strumLineNotes.members.length'), 1 do
 					if curBeat % 4 == i % 4 then
-						setPropertyFromGroup('strumLineNotes.members',i,'scale.x',0.9)
-						setPropertyFromGroup('strumLineNotes.members',i,'scale.y',0.9)
-						runHaxeCode('FlxTween.tween(game.strumLineNotes.members[' .. tostring(i) .. '].scale, {x: 0.7, y: 0.7}, Conductor.stepCrochet*0.004, {ease: FlxEase.circInOut});')
+						setPropertyFromGroup('strumLineNotes.members',i,'scale.x',0.825)
+						setPropertyFromGroup('strumLineNotes.members',i,'scale.y',0.825)
+						runHaxeCode('FlxTween.tween(game.strumLineNotes.members[' .. tostring(i) .. '].scale, {x: 0.7, y: 0.7}, Conductor.stepCrochet*0.004, {ease: FlxEase.cubeOut});')
 						--doTweenX('camGameX', 'camGame', -newAngle*5, crochet*0.0015, 'cubeInOut')
 					end
 				end
 			--end
 		elseif curBeat >= 228 and not beat228Event then 
+		
+			cancelTween('camHUDAngle')
+			cancelTween('camGameAngle')
+			cancelTween('camHUDZoom')
+			cancelTween('camGameZoom')
+		
 			--hardBeatHit(25,1.1)
 			beat228Event = true
 			setProperty('camZooming',true)
-			setProperty('camGame.zoom',0.85)
+			setProperty('camGame.zoom',0.75)
 			setProperty('camHUD.zoom',0.75)
 			doTweenX('camHUDX', 'camHUD', 0, crochet*0.001, 'cubeInOut')
 			doTweenX('camGameX', 'camGame', 0, crochet*0.001, 'cubeInOut')
