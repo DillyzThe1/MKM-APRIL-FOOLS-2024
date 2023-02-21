cccbe59d28eba7cecc14e5cfc8d88c78db27e454
DillyzThe1 <dyl1029an@gmail.com>
2023-02-20 21:55:46 -0600

    toad tutorial stuff

--- example_mods/data/tutorial/script.lua
+++ example_mods/data/tutorial/script.lua
@@ -10,3 +10,37 @@ end
 function noteMissPress()
 	loadSong('Academic Failure')
 end
+
+function onCreatePost()
+	setProperty("dad.visible",false)
+	setProperty("dad.active",false)
+	addLuaScript("custom_events/Change IconP2")
+	triggerEvent("Change IconP2", "gf", "A5004D")
+end
+
+local northerntexas = false
+function onBeatHit()
+	if curBeat >= 6 and not northerntexas then 
+		northerntexas = true
+		startCutscene()
+	end
+end
+
+function startCutscene()
+	triggerEvent("Alt Idle Animation", "gf", "-")
+	triggerEvent("Camera Follow Pos", "800", "200")
+	runTimer("gftrans", 10/24)
+end
+
+function onTimerCompleted(tag)
+	if tag == "gftrans" then
+		playAnim("gf", "transition", true)
+		runTimer("toadicon", 12/24)
+		runTimer("toadidle", 55/24)
+	elseif tag == "toadicon" then
+		triggerEvent("Change IconP2", "toad", "FF0033")
+	elseif tag == "toadidle" then
+		triggerEvent("Camera Follow Pos", "", "")
+		triggerEvent("Alt Idle Animation", "gf", "-alt")
+	end
+end
