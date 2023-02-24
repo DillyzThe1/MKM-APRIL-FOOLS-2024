1508c97d03098043be419a8ed4bd81f6af975716
DillyzThe1 <dyl1029an@gmail.com>
2023-02-23 23:49:10 -0600

    credits work

--- example_mods/scripts/songCredits.lua
+++ example_mods/scripts/songCredits.lua
@@ -1,30 +1,26 @@
 local creditedPeople = ''
 
 local creditsMatch = {
-	{'tutorial', 'Composed By KawaiSprite, Edited by DillyzThe1'},
-	{'house', 'Composed By DillyzThe1'},
-	{'house skill issue', 'Composed By DillyzThe1'},
-	{'no shrooms', 'Composed By DillyzThe1'},
-	{'no shrooms skill issue', 'Composed By DillyzThe1'},
-	{'chaotically stupid', 'Composed By That1LazerBoi & DillyzThe1'},
-	{'chaotically stupid skill issue', 'Composed By That1LazerBoi & DillyzThe1'},
-	{'bup', 'Composed By That1LazerBoi & DillyzThe1'},
-	{'bup skill issue', 'Composed By That1LazerBoi & DillyzThe1'},
-	{'extra screwed', 'Composed By DillyzThe1'},
-	{'extra screwed skill issue', 'Composed By DillyzThe1'},
-	{'welcom toad', 'Original Song By Novatos Team'},
-	{'welcom toad skill issue', 'Original Song By Novatos Team, Cover by DillyzThe1'},
 	{'academic failure', 'Composed by Zarzok'},
-	{'square', 'Composed by DillyzThe1'},
-	{'shroomus toodus', 'Original versions by Adam McHummus & Ethan The Doodler, Remixed by DillyzThe1'},
-	{'karrd kollision', 'Composed by DillyzThe1'},
+	{'bup', 'Composed By That1LazerBoi & DillyzThe1'},
+	{'chaotically stupid', 'Composed By That1LazerBoi & DillyzThe1'},
+	{'extra screwed', 'Composed By DillyzThe1'},
 	{'hell shrooms', 'Composed by DillyzThe1'},
+	{'house', 'Composed By DillyzThe1'},
+	{'incorrect residence', 'this song had me in tears (and it\'s by dillyz)'},
+	{'karrd kollision', 'Composed by DillyzThe1'},
+	{'normalized', 'Composed by DillyzThe1'},
+	{'no shrooms', 'Composed By DillyzThe1'},
+	{'shroomus toodus', 'Original versions by Adam McHummus & Ethan The Doodler, Remixed by DillyzThe1'},
+	{'square', 'Composed by DillyzThe1'},
+	{'top 10 great amazing super duper wonderful outstanding saster level music that ever has been heard', 'Composed by DillyzThe1'},
+	{'tutorial', 'Composed By KawaiSprite, Edited by DillyzThe1'},
+	{'welcom toad', 'Original Song By Novatos Team, Cover by DillyzThe1'},
 	{'wrong house', 'Composed by Impostor5875'},
-	{'normalized', 'Composed by DillyzThe1'}
+	{'yeah', 'Composed by DillyzThe1'}
 }
 
 function onCreatePost()
-
 	for i = 1, table.maxn(creditsMatch), 1 do
 		local creditsTable = creditsMatch[i]
 		if creditsTable[1] == string.lower(songName) then 
@@ -42,5 +38,12 @@ function onCreatePost()
 	
 	if getProperty('curStage') == 'minecraft' then 
 		setTextFont('creditsTxt','minecraft')
+	end
+end
+
+function onEvent(ev, v1, v2)
+	if ev == 'Change Credits' then 
+		setProperty('creditsTxt.text',v1)
+		setProperty('creditsTxt.x',getPropertyFromClass('flixel.FlxG','width')/2 - getTextWidth('creditsTxt')/2)
 	end
 end
