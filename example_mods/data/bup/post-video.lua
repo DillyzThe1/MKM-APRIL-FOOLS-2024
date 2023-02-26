6c56fe894ab7598a150f0840b919454676503cb5
DillyzThe1 <dyl1029an@gmail.com>
2023-02-25 22:38:18 -0600

    port bup alpha chart

--- example_mods/data/bup/post-video.lua
+++ example_mods/data/bup/post-video.lua
@@ -1,19 +1,23 @@
 local allowCountdown = false
 	
 function goingToDoCutscene()
-	if not allowCountdown and isStoryMode then --and not seenCutscene then
-		return true 
+	if not string.lower(difficultyName) == "alpha" then
+		if not allowCountdown and isStoryMode then --and not seenCutscene then
+			return true 
+		end
+			
+		return false
 	end
-		
-	return false
 end
 function onEndSong()
-	if goingToDoCutscene() then --Block the first countdown
-		setProperty('camGame.alpha',0)
-		setProperty('camHUD.alpha',0)
-		startVideo('post-' .. string.lower(songName));
-		allowCountdown = true;
-		return Function_Stop;
+	if not string.lower(difficultyName) == "alpha" then
+		if goingToDoCutscene() then --Block the first countdown
+			setProperty('camGame.alpha',0)
+			setProperty('camHUD.alpha',0)
+			startVideo('post-' .. string.lower(songName));
+			allowCountdown = true;
+			return Function_Stop;
+		end
+		return Function_Continue;
 	end
-	return Function_Continue;
 end
--- example_mods/data/bup/ratings.lua
+++ example_mods/data/bup/ratings.lua
@@ -2,31 +2,35 @@ local abortionLaws = 'Your mother should\'ve gotten the abortion.'
 local rating60hxFile = nil
 
 function onCreatePost()
-	addHaxeLibrary('Date')
-	rating60hxFile = getTextFromFile('data/bup/Rating60.hx',false)
+	if not string.lower(difficultyName) == "alpha" then
+		addHaxeLibrary('Date')
+		rating60hxFile = getTextFromFile('data/bup/Rating60.hx',false)
+	end
 end
 
 function onUpdatePost(e)
-	local ratingpercent = rating * 100
-	if ratingpercent >= 100 then 
-		abortionLaws = 'ok blue hair liberal'
-	elseif ratingpercent >= 90 then 
-		abortionLaws = 'get ur goofy ahh out of here'
-	elseif ratingpercent >= 75 then 
-		abortionLaws = 'Portal 2 Radio Loop (10 Hours)'
-	elseif ratingpercent >= 60 then 
-		--local timesTablesReference = os.date("*t")
-		runHaxeCode(rating60hxFile)
-		--abortionLaws = 'Execution Date: ' .. 
-	elseif ratingpercent >= 45 then 
-		abortionLaws = 'Did you forget to press the keys again??'
-	elseif ratingpercent >= 30 then 
-		abortionLaws = 'WE HAVE COME FOR YOUR LIVER'
-	elseif ratingpercent >= 15 then 
-		abortionLaws = 'Get out of here, you funky friday fan.'
+	if not string.lower(difficultyName) == "alpha" then
+		local ratingpercent = rating * 100
+		if ratingpercent >= 100 then 
+			abortionLaws = 'ok blue hair liberal'
+		elseif ratingpercent >= 90 then 
+			abortionLaws = 'get ur goofy ahh out of here'
+		elseif ratingpercent >= 75 then 
+			abortionLaws = 'Portal 2 Radio Loop (10 Hours)'
+		elseif ratingpercent >= 60 then 
+			--local timesTablesReference = os.date("*t")
+			runHaxeCode(rating60hxFile)
+			--abortionLaws = 'Execution Date: ' .. 
+		elseif ratingpercent >= 45 then 
+			abortionLaws = 'Did you forget to press the keys again??'
+		elseif ratingpercent >= 30 then 
+			abortionLaws = 'WE HAVE COME FOR YOUR LIVER'
+		elseif ratingpercent >= 15 then 
+			abortionLaws = 'Get out of here, you funky friday fan.'
+		end
+		setRatingName(abortionLaws)
+		setProperty('botplayTxt.text', 'FEMALE REPELLANT')
 	end
-	setRatingName(abortionLaws)
-	setProperty('botplayTxt.text', 'FEMALE REPELLANT')
 end
 
 function updateTextManuallyLmao(getreal)
