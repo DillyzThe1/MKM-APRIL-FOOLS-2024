9b7411af8c1499b7f14f049d560baf4e29bf636e
Doveryat <36282680+Zarzok648GD@users.noreply.github.com>
2023-03-04 12:55:57 -0600

    wouldn't it be funny if

--- example_mods/data/incorrect-residence/cheat.lua
+++ example_mods/data/incorrect-residence/cheat.lua
@@ -2,6 +2,9 @@ function onChartAccessed()
 	if songCompletedOnDiff('Hell Shrooms', 'Hard') then
 		loadSong('Hell Shrooms', 'Impostor Top 10')
 		return Function_Stop
+	else
+		loadSong('why')
+		sreturn Function_Stop
 	end
 	return Function_Continue
 end
