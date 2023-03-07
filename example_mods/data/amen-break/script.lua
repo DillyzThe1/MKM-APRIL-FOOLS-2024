6600a2983ebec2df7b2f68b553acf225d1e4f7f1
DillyzThe1 <dyl1029an@gmail.com>
2023-03-07 11:47:35 -0600

    no

--- example_mods/data/amen-break/script.lua
+++ example_mods/data/amen-break/script.lua
@@ -1,28 +1,13 @@
-local allowCountdown = false
-
-function goingToDoCutscene()
-	if not allowCountdown then --and not seenCutscene then
-		return true 
-	end
-		
-	return false
-end
-
 function onCreatePost()
-	if goingToDoCutscene() then 
-		setProperty('camGame.alpha',0)
-	end
-	debugPrint(allowFullscreen)
+	setProperty('camGame.alpha',0)
 end
 
 function onStartCountdown()
-	if goingToDoCutscene() then --Block the first countdown
-		startVideo('Amen');
-		setPropertyFromClass('flixel.FlxG','fullscreen',true)
-		allowCountdown = true;
-		return Function_Stop;
+	if not allowCountdown then --Block the first countdown
+		startVideo('Amen')
+		allowCountdown = true
+		return Function_Stop
 	end
-	setPropertyFromClass('flixel.FlxG','fullscreen',false)
 	endSong()
 	return Function_Stop;
 end
