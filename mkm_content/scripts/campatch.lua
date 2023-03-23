6c15083aac8d543c8cd58129e8279eeaa4e6fd15
DillyzThe1 <34419642+DillyzThe1@users.noreply.github.com>
2023-03-23 11:18:24 -0500

    fix tutorial

--- mkm_content/scripts/campatch.lua
+++ mkm_content/scripts/campatch.lua
@@ -3,7 +3,13 @@ function onUpdatePost(elapsed)
 		if mustHitSection then
 			cameraSetTarget('bf')
 		else 
-			cameraSetTarget('dad')
+			if gfSection then
+				runHaxeCode("game.camFollow.set(game.gf.getMidpoint().x, game.gf.getMidpoint().y);")
+				runHaxeCode("game.camFollow.x += game.gf.cameraPosition[0] + game.girlfriendCameraOffset[0];")
+				runHaxeCode("game.camFollow.y += game.gf.cameraPosition[1] + game.girlfriendCameraOffset[1];")
+			else
+				cameraSetTarget('dad')
+			end
 		end
 	end
 end
