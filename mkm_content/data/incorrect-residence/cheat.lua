559ae8e690f3fdddef0ad2380ab636feeb4c6b4d
DillyzThe1 <34419642+DillyzThe1@users.noreply.github.com>
2024-01-08 18:11:35 -0600

    rest in shrooms my fellow song

--- mkm_content/data/incorrect-residence/cheat.lua
+++ mkm_content/data/incorrect-residence/cheat.lua
@@ -1,9 +1,4 @@
 function onChartAccessed()
-	if songCompletedOnDiff('Hell Shrooms', 'Hard') and not songCompletedOnDiff('Hell Shrooms', 'Impostor Top 10') then
-		loadSong('Hell Shrooms', 'Impostor Top 10')
-		debugPrint("hell shrooms")
-		return Function_Stop
-	end
 	loadSong('why', 'Hard')
 	return Function_Stop
 end
