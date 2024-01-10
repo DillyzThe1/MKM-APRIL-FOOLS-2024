24345087857a6d72c9620b5754020fa171a2668d
DillyzThe1 <34419642+DillyzThe1@users.noreply.github.com>
2024-01-09 21:54:53 -0600

    temporary sponge stuff

---
+++ mkm_content/stages/toadHouse-st.lua
@@ -0,0 +1,22 @@
+function onCreatePost()
+	makeLuaSprite('wall','bgs/house/wall lmao spongeless',-750,-150)
+	addLuaSprite('wall',false)
+	setProperty('wall.antialiasing',true)
+	setProperty('wall.active',false)
+	
+	makeLuaSprite('floor','bgs/house/floor wood',-600,650)
+	addLuaSprite('floor',false)
+	setProperty('floor.antialiasing',true)
+	setProperty('floor.active',false)
+	
+	makeLuaSprite('carpte','bgs/house/Carpet',320,725)
+	addLuaSprite('carpte',false)
+	setProperty('carpte.antialiasing',true)
+	setProperty('carpte.active',false)
+	
+	makeLuaSprite('floor line','bgs/house/floor line thing',-1050,650)
+	addLuaSprite('floor line',false)
+	setProperty('floor line.antialiasing',true)
+	setProperty('floor line.active',false)
+	
+end
