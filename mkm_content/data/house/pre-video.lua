b7324cc5e84627ca5d27a00b5041fd1f7fc59204
DillyzThe1 <34419642+DillyzThe1@users.noreply.github.com>
2024-03-12 02:06:37 -0500

    new pre-house cutscene

---
+++ mkm_content/data/house/pre-video.lua
@@ -0,0 +1,37 @@
+local allowCountdown = false
+	
+function goingToDoCutscene()
+	if isInANormalMode() and not allowCountdown and isStoryMode and not seenCutscene then --and not seenCutscene then
+		return true 
+	end
+		
+	return false
+end
+
+function onCreatePost()
+	if goingToDoCutscene() then 
+		setProperty('camGame.alpha',0)
+	end
+end
+
+function onStartCountdown()
+	if goingToDoCutscene() then --Block the first countdown
+		if string.lower(difficultyName) == "alpha" then
+			startVideo('pre-house-alpha');
+		else
+			startVideo('pre-' .. string.lower(songName));
+		end
+		allowCountdown = true;
+		return Function_Stop;
+	end
+	setProperty('camGame.alpha',1)
+	return Function_Continue;
+end
+
+function isInANormalMode()
+	local diffff = string.lower(difficultyName)
+	if diffff == "hard" or diffff == "old" or diffff == "alpha" then
+		return true
+	end
+	return false
+end
